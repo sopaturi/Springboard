9231da7a76b782e41bf5e1bac47c97ca88eaf1f1
Soorya <sopaturi@gmail.com>
2018-08-13 11:09:40 -0700

    Delete SQL login info

--- SQL/1520094343_sql_project.sql
+++ SQL/1520094343_sql_project.sql
@@ -1,10 +1,5 @@
 /* Welcome to the SQL mini project. For this project, you will use
-Springboard' online SQL platform, which you can log into through the
-following link:
-
-https://sql.springboard.com/
-Username: student
-Password: learn_sql@springboard
+Springboard' online SQL platform.
 
 The data you need is in the "country_club" database. This database
 contains 3 tables:
